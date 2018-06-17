44daadb469c9b314245a1a21c16d86a55061e8d2
Franverri <franverri@hotmail.com>
2018-06-17 00:50:36 -0300

    Pantalla items famoso

--- app/src/main/java/tdp/vip/ItemsActivity.java
+++ app/src/main/java/tdp/vip/ItemsActivity.java
@@ -1,11 +1,16 @@
 package tdp.vip;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class ItemsActivity extends AppCompatActivity {
+
+    private Button btnFollow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +21,19 @@ public class ItemsActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_items);
+
+        //Obtengo la referencia al boton de "Seguir"
+        btnFollow = (Button) findViewById(R.id.btnFollow);
     }
+
+    public void followFamous(View view) {
+        if(btnFollow.getText().equals("Seguir")){
+            btnFollow.setText("Seguido");
+            btnFollow.setBackgroundDrawable(getResources().getDrawable(R.drawable.roundedbuttongreen));
+        } else {
+            btnFollow.setText("Seguir");
+            btnFollow.setBackgroundDrawable(getResources().getDrawable(R.drawable.roundedbutton));
+        }
+    }
+
 }
