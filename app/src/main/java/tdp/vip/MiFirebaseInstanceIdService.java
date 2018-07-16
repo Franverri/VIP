c1b2833b4852a1da51a078449c10c32cf9599eb3
Franverri <franverri@hotmail.com>
2018-07-16 14:46:42 -0300

    Notificaciones desde Firebase

---
+++ app/src/main/java/tdp/vip/MiFirebaseInstanceIdService.java
@@ -0,0 +1,20 @@
+package tdp.vip;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MiFirebaseInstanceIdService extends FirebaseInstanceIdService {
+
+    public static final String TAG = "NOTICIAS";
+
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+
+        String token = FirebaseInstanceId.getInstance().getToken();
+
+        Log.d(TAG, "Token: " + token);
+    }
+}
---
+++ app/src/main/java/tdp/vip/MiFirebaseMessagingService.java
@@ -0,0 +1,46 @@
+package tdp.vip;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MiFirebaseMessagingService extends FirebaseMessagingService {
+
+    public static final String TAG = "NOTICIAS";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        String from = remoteMessage.getFrom();
+        Log.d(TAG, "Mensaje recibido de: " + from);
+
+        if(remoteMessage.getNotification() != null){
+            Log.d(TAG, "Notificación: " + remoteMessage.getNotification().getBody());
+            mostrarNotificacion(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+        }
+    }
+
+    private void mostrarNotificacion(String title, String body){
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.icon)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+}
